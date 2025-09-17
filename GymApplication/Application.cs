8585ead7f6eef2d9cbb82059ee7d398247cd3d03
RyugaWork <buidinhlongwork@gmail.com>
2025-09-17 19:19:34 +0700

    feat: add Logger dll and make change to .gitignore Add custom Logger Add(.gitignore)  ignore build artifacts

--- GymApplication/Application.cs
+++ GymApplication/Application.cs
@@ -1,20 +1,19 @@
-﻿
-class Program {
+﻿class Program {
     [STAThread]
     static async Task Main(string[] args) {
-        Logger.Core.Logger.Trace().Cid("Trace").Log("");
-        Logger.Core.Logger.Critical().Cid("Critical").Log("");
-        Logger.Core.Logger.Debug().Cid("Debug").Log("");
-        Logger.Core.Logger.Error().Cid("Error").Log("");
-        Logger.Core.Logger.Warn().Cid("Warn").Log("");
-        Logger.Core.Logger.Info().Cid("Info").Log("");
+        Logger.Logger.Trace().Cid("Trace").Log("");
+        Logger.Logger.Critical().Cid("Critical").Log("");
+        Logger.Logger.Debug().Cid("Debug").Log("");
+        Logger.Logger.Error().Cid("Error").Log("");
+        Logger.Logger.Warn().Cid("Warn").Log("");
+        Logger.Logger.Info().Cid("Info").Log("");
 
         // Simulate running app with graceful exit on Ctrl+C
         Console.CancelKeyPress += (s, e) => {
             Environment.Exit(0);
         };
         while (true) {
-            Logger.Core.Logger.Info().Log("Client is running - Ctr + C to exit");
+            Logger.Logger.Info().Log("Client is running - Ctr + C to exit");
             await Task.Delay(60000);
         }
     }
---
+++ Logger/Logger.cs
@@ -0,0 +1,14 @@
+﻿using Logger.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Logger;
+public static class Logger {
+    public static Builder Trace() => new Builder(LogLevel.Trace);
+    public static Builder Critical() => new Builder(LogLevel.Critical);
+    public static Builder Info() => new Builder(LogLevel.Information);
+    public static Builder Warn() => new Builder(LogLevel.Warning);
+    public static Builder Error() => new Builder(LogLevel.Error);
+    public static Builder Debug() => new Builder(LogLevel.Debug);
+}
+
+
